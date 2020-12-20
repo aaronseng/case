cb4dab441750a45b8278edd73e7212056c4e9bb3
aaron seng <seng.aaron@gmail.com>
2020-12-21 01:25:35 +0300

    LoadingScene is added.

---
+++ Assets/Scripts/Game/Managers/LoadingManager.cs
@@ -0,0 +1,59 @@
+﻿using Case.Core;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Case.Scene.Manager
+{
+
+    /// <summary>
+    /// We make all light-weight object pool creations, loading terrains, multiplayer network connections in LoadingScene.
+    /// Also Game Hud preparations are good candidate to be done in LoadingScene as well.
+    /// For this study case I'll just implement light-weight Object Pool initialization in LoadingScene.
+    /// </summary>
+    public class LoadingManager : MonoBehaviour
+    {
+        [SerializeField]
+        private Image _progressBar;
+
+        private bool isGameLoading = false;
+
+        #region Unity Methods
+
+        private async void Update()
+        {
+            // Update the progressbar with the current object pool progress.
+            _progressBar.fillAmount = ObjectPoolManager.Instance.Progress;
+
+            // Load the game scene when object pool is ready.
+            if (ObjectPoolManager.Instance.IsReady && !isGameLoading)
+            {
+                isGameLoading = true;
+                await loadGameSceneAsync();
+            }
+        }
+
+        #endregion
+
+        #region Manager Logic
+
+        /// <summary>
+        /// Loads the GameScene asynchronously.
+        /// </summary>
+        /// <returns></returns>
+        private async Task loadGameSceneAsync()
+        {
+            AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("GameScene");
+
+            // Wait until the asynchronous scene fully loads
+            while (!asyncLoad.isDone)
+            {
+                await Task.Delay(System.TimeSpan.FromMilliseconds(10));
+            }
+        }
+
+        #endregion
+
+    }
+}
